ef709460b1dcb67410b076e787e63dacd036edd7
Craig Drabik <craig.drabik@txmq.com>
2018-01-25 09:59:01 -0500

    Non-subtree commits related to socket API refactor debugging

--- hashgraph workspace/hashgraph/source/socketdemo/src/com/txmq/socketdemo/socket/ZooSocketApi.java
+++ hashgraph workspace/hashgraph/source/socketdemo/src/com/txmq/socketdemo/socket/ZooSocketApi.java
@@ -1,6 +1,7 @@
 package com.txmq.socketdemo.socket;
 
 import com.txmq.exo.core.ExoPlatformLocator;
+import com.txmq.exo.core.ExoState;
 import com.txmq.exo.messaging.ExoMessage;
 import com.txmq.exo.messaging.ExoTransactionType;
 import com.txmq.exo.transactionrouter.ExoTransaction;
@@ -12,12 +13,12 @@ import io.swagger.model.Zoo;
 public class ZooSocketApi {
 
 	@ExoTransaction(SocketDemoTransactionTypes.GET_ZOO)
-	public ExoMessage getZoo() {
-		SocketDemoState state = (SocketDemoState) ExoPlatformLocator.getState();
+	public ExoMessage getZoo(ExoMessage message, ExoState state) {
+		SocketDemoState _state = (SocketDemoState) ExoPlatformLocator.getState();
 		Zoo result = new Zoo();
-		result.lions(state.getLions());
-		result.tigers(state.getTigers());
-		result.bears(state.getBears());
+		result.lions(_state.getLions());
+		result.tigers(_state.getTigers());
+		result.bears(_state.getBears());
 		
 		return new ExoMessage(
 			new ExoTransactionType(ExoTransactionType.ACKNOWLEDGE),
