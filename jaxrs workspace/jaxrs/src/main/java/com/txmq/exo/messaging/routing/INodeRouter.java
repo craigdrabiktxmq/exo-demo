dcc773409815bd476910f674c1b47204d7189f4e
Craig Drabik <craig.drabik@txmq.com>
2018-01-29 11:48:40 -0500

    removing java-client subtree so I can repoint it

--- jaxrs workspace/jaxrs/src/main/java/com/txmq/exo/messaging/routing/INodeRouter.java
+++
@@ -1,9 +0,0 @@
-package com.txmq.exo.messaging.routing;
-
-import java.net.SocketAddress;
-import java.util.List;
-
-public interface INodeRouter {
-	public void setAvailableNodes(List<SocketAddress> nodes);
-	public SocketAddress getNode();
-}
